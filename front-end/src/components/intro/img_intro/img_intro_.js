8e2d5afbcbfe67f0f1cac7fda3c1be342c4f031b
khoa16122004 <khoatr.work@gmail.com>
2024-09-28 15:08:47 +0700

    responsive

--- front-end/src/components/intro/img_intro/img_intro_.js
+++ front-end/src/components/intro/img_intro/img_intro_.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './img_intro.modul.css';
+import img from "./llama.png";
 
 const Image = () => {
     return (
-        <div className="Image">
+        <div className="Right">
+            <img src={img}/>
+                <div className="text">
+                    <div>
+                        <div class="text_">The world </div>
+                        <div class="text_">at your fingertips!</div>
+                    </div>
+                      
+            </div>
             
         </div>
     )
 }
 export default Image;
+
--- front-end/src/components/intro/text_intro/text_intro_.js
+++ front-end/src/components/intro/text_intro/text_intro_.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './text_intro.modul.css';
+import {Link} from 'react-router-dom';
 
 const Text = () => {
     return (
         <div className="left">
-            <span style={{ fontWeight: 'bold', fontSize: '60px' }}>We are Coursey!</span> <br/>
-            <span style={{ fontSize: '60px', marginLeft: '20px'}}>1500+</span> 
-            <span style={{ fontSize: '20px' , marginLeft: '10px'}}>learners every day</span> <br/>
+            <span style={{ fontWeight: 'bold', fontSize: '80px' }}>We are Coursey!</span> <br/>
+            <span style={{ fontSize: '70px', marginLeft: '20px'}}>1500+</span> 
+            <span style={{ fontSize: '30px' , marginLeft: '10px'}}>learners every day</span> <br/>
             <div className="btn">Join with us now</div>
         </div>
     )
