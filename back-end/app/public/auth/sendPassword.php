c0e1e38d1a701c5780015afac0fbca0801bb5743
khoa16122004 <khoatr.work@gmail.com>
2024-12-18 00:15:23 +0700

    Update sendPassword.php

--- back-end/app/public/auth/sendPassword.php
+++ back-end/app/public/auth/sendPassword.php
@@ -1,10 +1,9 @@
 <?php
-
+    include '../../cors/cors.php'; 
     require_once '/var/www/html/vendor/autoload.php';
-
     require_once '../../db/usersControlers.php';
 
-    header('Content-Type: application/json');
+    // header('Content-Type: application/json');
     $userController = new UserController();
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
